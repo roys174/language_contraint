31eb536485ae9a572551d36b325d15f0647c9b78
Roy Schwartz <roysch@164.7.208.128.dynr.cs.washington.edu>
2017-02-02 20:22:52 -0800

    Roy V3 (not final)

--- ROC_SVM.py
+++ ROC_SVM.py
@@ -20,6 +20,7 @@ def read_features(ifile, n_feats):
         
             # print d,line
             labels.append(d[0])
+            # print d[0],d[1]
         
             fs = d[2].split(" ")
         
@@ -40,8 +41,14 @@ def read_features(ifile, n_feats):
     
     return [features, labels]
     
-def evaluate(test,labels,confidence = []):
+def evaluate(test,labels,confidence = [], ids = None, gold = None, ofile = None):
     n_correct = 0
+    
+    if (ofile != None):
+        ofh = FileTools.openWriteFile(ofile)
+        ofh.write("InputStoryid,AnswerRightEnding\n")
+        
+    
     for i in range(len(labels)):
         # print labels[i],test[i]
         n_correct += (labels[i]==test[i])
@@ -51,7 +58,7 @@ def evaluate(test,labels,confidence = []):
 
     if (len(confidence)):    
         n_pairs_correct = 0
-    
+
         replace = {'2':'1', '1':'2'}
         for i in range(len(labels)/2):
             if (test[2*i] == test[2*i+1]):
@@ -60,13 +67,28 @@ def evaluate(test,labels,confidence = []):
                 else:
                     test[2*i] = replace[test[2*i]]
             
+            is_correct = (labels[2*i]==test[2*i])
+            
+            if ((is_correct and not (labels[2*i+1]==test[2*i+1])) or (not is_correct and (labels[2*i+1]==test[2*i+1]))):
+                print "Big problem!!!"
+                
             # print labels[i],test[i]
-            n_pairs_correct += (labels[2*i]==test[2*i])
+            n_pairs_correct += is_correct
             n_pairs_correct += (labels[2*i+1]==test[2*i+1])
     #        print labels[i],test[i],confidence[i]
+
+            if (ofile != None):
+                v = gold[i]
+                if (not is_correct):
+                    v = 3-v
+                    
+                ofh.write(ids[i]+","+str(v)+"\n")
     
         print n_pairs_correct,"/",len(labels),"=",n_pairs_correct*1./len(labels)
 
+    if (ofile != None):
+        ofh.write("\n")
+        ofh.close()
 
 def gen_ranking(features, labels):
     features2 = []
@@ -84,5 +106,5 @@ def gen_ranking(features, labels):
         features2.append(diff.tolist())
         labels2.append(label)
 
-    print len(features2),len(labels2)
+    #print len(features2),len(labels2)
     return [features2, labels2]
--- test_SVM.py
+++ test_SVM.py
@@ -3,6 +3,7 @@
 import sys
 sys.path.insert(0, '/Users/roysch/aristo/entailment/scripts/')
 import ROC_SVM
+import FileTools
 from scipy.sparse import *
 from scipy import *
 from sklearn import svm
@@ -11,14 +12,22 @@ import pickle
 
 def main():
     do_rank = 0
-    if (len(sys.argv) < 4):
-        print "Usage:",sys.argv[0],"<if> <n_feats> <model if> <is ranking>" 
+    ids=None
+    gold=None
+    out_file = None
+    if (len(sys.argv) < 3):
+        print "Usage:",sys.argv[0],"<if> <model if> <is ranking> <mapping file> <out file>" 
         return -1
-    elif (len(sys.argv) > 4):
-        do_rank = bool(sys.argv[4])
+    elif (len(sys.argv) > 3):
+        do_rank = int(sys.argv[3])
+        if (len(sys.argv) > 4):
+            mapping_file = sys.argv[4]
+            ids,gold = read_mapping_file(mapping_file)
+            if (len(sys.argv) > 5):
+                out_file = sys.argv[5]
 
-    n_feats = int(sys.argv[2])
-    clf = pickle.load(open(sys.argv[3], 'r'))
+    clf = pickle.load(open(sys.argv[2], 'r'))
+    n_feats = len(clf.coef_[0])
     
     features = []
     labels = []
@@ -37,7 +46,25 @@ def main():
         confidence = clf.decision_function(features)
     
     
-    ROC_SVM.evaluate(test,labels,confidence)
+    ROC_SVM.evaluate(test,labels,confidence,ids,gold, out_file)
     
+
+def read_mapping_file(ifile):
+    ids = []
+    gold = []
+    with FileTools.openReadFile(ifile) as ifh:
+        # Ignore first lione
+        ifh.readline()
+
+        for line in ifh:
+            e = line.rstrip().split("\t");
+            id = e[0]
+            ids.append(id)
+            gold.append(int(e[-1]))
+	
+        ifh.close()
+
+	return ids,gold
+
 
 sys.exit(main())
